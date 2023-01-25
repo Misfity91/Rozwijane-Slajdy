8d8beb841ecee6eabda4c633fb000162a3037617
Rafał Chundzyński <99214915+Misfity91@users.noreply.github.com>
2023-01-25 20:41:18 +0100

    End project

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body')
+const cards = document.querySelectorAll('.card')
+
+function showcard() {
+    cards.forEach( card => {
+        card.classList.remove('active')
+        this.classList.add('active')
+       
+    })
+     handleBgColor(this)
+}
+
+const handleBgColor = (card) => {
+    const season = card.getAttribute('data-season')
+    body.setAttribute('data-bg', season)
+}
+
+
+cards.forEach(card => card.addEventListener("click", showcard))
